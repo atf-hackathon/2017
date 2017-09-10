af7a97bbc56bb08f435e170120de2ce875d56e67
Ionut Marinescu <ionut.marinescu@emag.ro>
2017-09-10 05:09:49 +0300

    Checking boxability on a whitelist in the user script

--- greasemonkey.js
+++ greasemonkey.js
@@ -28,8 +28,31 @@
         }
 
         console.log('Ready!');
+        console.log('Checking boxability...');
 
-        deliveryEstimatePanel.append('<div class="delivery-estimate-border-btm"><div class="delivery-estimate-box pad-sep-xs"><i class="delivery-estimate-icon em em-open"></i><div><div>eBox instant</div><span class="text-success delivery-estimate-text-sm">livrare automata instantanee</span></div></div></div>');
+        var boxable = false;
+        var boxableProductCodes = ['DY3MHDBBM','D3SBN2BBM','DLXM4KBBM','D1XVG7BBM','DSQD33BBM','DFXR03BBM','DRXVG7BBM','DPTK07BBM','DKJB4KBBM','DPDVF2BBM','DNDVF2BBM','DMSQKMBBM','DPL63MBBM','DX90QBBBM','DKQH5YBBM','DN0G1BBBM','D0HQXBBBM','DX5L07BBM','DP0TCBBBM','DPTL42BBM','DJFNS7BBM','D2WS8YBBM','DSY023BBM','DNJGZ3BBM','DY2JV2BBM','DTY0RBBBM','DNL6X2BBM','DLZGDDBBM','DJQT0MBBM','D0R5SBBBM'];
+        for (var i in boxableProductCodes) {
+            if (boxableProductCodes[i] == productCode) {
+                boxable = true;
+                break;
+            }
+        }
+        if (boxable) {
+            console.log('Boxable it is!');
+            deliveryEstimatePanel.append(
+                '<div class="delivery-estimate-border-btm">'+
+                    '<div class="delivery-estimate-box pad-sep-xs">'+
+                        '<i class="delivery-estimate-icon em em-open"></i>'+
+                        '<div>'+
+                            '<div>eBox instant</div><span class="text-success delivery-estimate-text-sm">livrare automata instantanee</span>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+            );
+        } else {
+            console.log('Not boxable at this time!');
+        }
     }
 
     setTimeout(checkDeliveryEstimatePanel, 2000);
